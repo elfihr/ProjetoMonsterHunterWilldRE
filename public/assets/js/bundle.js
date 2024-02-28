19ff00feac40db35aee28614eff86602d9ccf4c5
Luiz Felipe <70807899+elfihr@users.noreply.github.com>
2024-02-27 21:40:03 -0300

    Initial commit

---
+++ public/assets/js/bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+//ola
+console.log('ola mundo');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
